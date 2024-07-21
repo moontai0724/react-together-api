168662e95d2013b52376137e1912e59174515c46
moontai0724 <moontai0724@gmail.com>
2024-07-21 13:15:39 +0800

    feat(db): create flickr-photos

---
+++ migrations/20240721045415_create-flickr-photos.ts
@@ -0,0 +1,22 @@
+import type { Kysely } from "kysely";
+
+import { withTimestamps } from "./common";
+
+export async function up(db: Kysely<unknown>): Promise<void> {
+  await db.schema
+    .createTable("flickr_photos")
+    // Flickr photo ID
+    .addColumn("id", "bigint", (col) => col.primaryKey())
+    // Flickr photo page
+    .addColumn("url", "varchar(100)", (col) => col.notNull())
+    // Photo taken time from Flickr
+    .addColumn("taken_at", "datetime", (col) => col.notNull())
+    // Photo uploaded time from Flickr
+    .addColumn("uploaded_at", "datetime", (col) => col.notNull())
+    .$call(withTimestamps)
+    .execute();
+}
+
+export async function down(db: Kysely<unknown>): Promise<void> {
+  await db.schema.dropTable("flickr_photos").execute();
+}
