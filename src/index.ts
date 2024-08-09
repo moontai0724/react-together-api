3cf107c3d715f0ffd41f0945fe6b7771010e86c6
moontai0724 <moontai0724@gmail.com>
2024-08-09 22:56:14 +0800

    fix: allow cors anywhere

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { resolve } from "node:path";
 
 import fastifyAutoload from "@fastify/autoload";
+import fastifyCors from "@fastify/cors";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
 import Fastify from "fastify";
@@ -12,6 +13,10 @@ export function buildApp(options: FastifyOptions = {}) {
   const app = Fastify(options as never);
 
   app.register(fastifySwagger, openapiOptions);
+
+  app.register(fastifyCors, {
+    origin: "*",
+  });
 
   app.register(fastifyAutoload, {
     dir: resolve(import.meta.dirname, "routes"),
